69a4cc33cd2a808d25262001d207617415d96e58
CreativeSelf1 <fernacastillo.perez0607@gmail.com>
2022-11-30 15:16:25 -0300

    ajustes pagina mapa

--- pages/mapUI.js
+++ pages/mapUI.js
@@ -1,75 +1,82 @@
 import { Box, Flex } from "@chakra-ui/react";
-import { Image, 
+import {
+    Image,
     Accordion,
     AccordionItem,
     AccordionButton,
     AccordionPanel,
     AccordionIcon
-        } from '@chakra-ui/react'
+} from '@chakra-ui/react'
 
 
 const MapUI = () => {
     return (
-        <Flex>
-            <Box >
-                <Image src='https://www.acusticauach.cl/wp-content/uploads/2012/10/Campus_Miraflores.jpg'
-                    paddingTop="70px"
-                    paddingBottom="150px"
-                    paddingLeft="70px"
-                />
-            </Box>
-            <Box>
-            <Box px="200px" py="100px"  border="1px " >
-                    <Accordion allowToggle>
-                        <AccordionItem>
-                            <h2>
-                                <AccordionButton>
-                                    <Box flex='1' textAlign='left'>
-                                        Edificio 10000
-                                    </Box>
-                                    <AccordionIcon />
-                                </AccordionButton>
-                            </h2>
-                            <AccordionPanel pb={4}>
-                            aqui se muestran las Salas
-                            del edicios
-                            </AccordionPanel>
-                        </AccordionItem>
-
-                        <AccordionItem>
-                            <h2>
-                                <AccordionButton>
-                                    <Box flex='1' textAlign='left'>
-                                        Edificio 9000
-                                    </Box>
-                                    <AccordionIcon />
-                                </AccordionButton>
-                            </h2>
-                            <AccordionPanel pb={4}>
-                                aqui se muestran las Salas
-                                del edicios
-                            </AccordionPanel>
-                        </AccordionItem>
-
-                        <AccordionItem>
-                            <h2>
-                                <AccordionButton>
-                                    <Box flex='1' textAlign='left'>
-                                        Edificio 8000
-                                    </Box>
-                                    <AccordionIcon />
-                                </AccordionButton>
-                            </h2>
-                            <AccordionPanel pb={4}>
-                                aqui se muestran las Salas
-                                del edicios
-                            </AccordionPanel>
-                        </AccordionItem>
-                    </Accordion>
-
+        <div class="mapUi">
+            <Flex>
+                <Box >
+                    <Image src='https://www.acusticauach.cl/wp-content/uploads/2012/10/Campus_Miraflores.jpg'
+                        paddingTop="70px"
+                        paddingBottom="150px"
+                        paddingLeft="70px"
+                    />
                 </Box>
-            </Box>
-        </Flex>
+                <Flex px="200px" py="100px">
+                    <div>
+                        <h2>Edificios</h2>
+                        <div>
+                            <Box paddingTop="20px">
+                                <Accordion allowToggle border="1px" w="300px" borderColor="green">
+                                    <AccordionItem>
+                                        <h2>
+                                            <AccordionButton>
+                                                <Box flex='1' textAlign='left'>
+                                                    Edificio 10000
+                                                </Box>
+                                                <AccordionIcon />
+                                            </AccordionButton>
+                                        </h2>
+                                        <AccordionPanel pb={4}>
+                                            aqui se muestran las Salas
+                                            del edicios
+                                        </AccordionPanel>
+                                    </AccordionItem>
+
+                                    <AccordionItem>
+                                        <h2>
+                                            <AccordionButton>
+                                                <Box flex='1' textAlign='left'>
+                                                    Edificio 9000
+                                                </Box>
+                                                <AccordionIcon />
+                                            </AccordionButton>
+                                        </h2>
+                                        <AccordionPanel pb={4}>
+                                            aqui se muestran las Salas
+                                            del edicios
+                                        </AccordionPanel>
+                                    </AccordionItem>
+
+                                    <AccordionItem>
+                                        <h2>
+                                            <AccordionButton>
+                                                <Box flex='1' textAlign='left'>
+                                                    Edificio 8000
+                                                </Box>
+                                                <AccordionIcon />
+                                            </AccordionButton>
+                                        </h2>
+                                        <AccordionPanel pb={4}>
+                                            aqui se muestran las Salas
+                                            del edicios
+                                        </AccordionPanel>
+                                    </AccordionItem>
+                                </Accordion>
+                            </Box>
+                        </div>
+                    </div>
+                </Flex>
+            </Flex>
+        </div>
     );
 }
 export default MapUI;
